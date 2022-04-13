73dd1af5bfbf7da819c48da5539e7629239736fe
simoneancona04 <simone.ancona.2004@calvino.edu.it>
2022-04-13 17:17:39 +0200

    Update woking

--- RBAC/src/File.java
+++ RBAC/src/File.java
@@ -1,3 +1,3 @@
-public class File {
-    
+public class File extends SysElement {
+
 }
--- RBAC/src/SysElement.java
+++ RBAC/src/SysElement.java
@@ -1,3 +1,20 @@
 public class SysElement {
-    
+    private AccessTable act;
+    private String name;
+
+    public SysElement() {
+        this.name = new String();
+        this.act = new AccessTable();
+    }
+
+    public SysElement(String name, AccessTable act) {
+        this.name = name;
+        this.act = act;
+    }
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public AccessTable getAct() {return act;}
+    public void setAct(AccessTable act) {this.act = act;}
 }
