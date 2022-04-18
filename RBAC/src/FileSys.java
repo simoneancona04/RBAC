35fef2b44e6de353bc606cf978cd9c257e9274c4
simoneancona04 <simone.ancona.2004@calvino.edu.it>
2022-04-18 22:26:37 +0200

    modifiche filesys.java

--- RBAC/src/FileSys.java
+++ RBAC/src/FileSys.java
@@ -1,11 +1,13 @@
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class FileSys {
     private Directory home; // chiamo home la directory principale che conterrà tutto
-    
+    private final String filename = "fileSys.json"
+
     public FileSys() {
         ObjectMapper om = new ObjectMapper();
-        
+        om.readValue(new File(filename), Directory.class);
     }
 
     public Directory getHome() {
