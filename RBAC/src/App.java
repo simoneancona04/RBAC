62c39fd662c927f4dbcbeadce89cdd5c73169a10
simone <simonechiarlo1@gmail.com>
2022-04-18 22:22:58 +0200

    Update

--- RBAC/src/App.java
+++ RBAC/src/App.java
@@ -1,6 +1,8 @@
 public class App {
+    static Entity currentEntity;
     public static void main(String[] args) {
         FileSys fileSys = new FileSys();
+        currentEntity = new Entity("mimmo","123");
         
     }
 }
