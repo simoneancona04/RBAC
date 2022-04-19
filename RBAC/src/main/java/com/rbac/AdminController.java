3b8fcad91be52c3a45c4531782d4f44052c94ddd
simone <simonechiarlo1@gmail.com>
2022-04-19 23:14:48 +0200

    update

--- RBAC/src/main/java/com/rbac/AdminController.java
+++ RBAC/src/main/java/com/rbac/AdminController.java
@@ -2,10 +2,27 @@ package com.rbac;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 
 public class AdminController {
     @FXML
     private void switchWindow() throws IOException { // servisse
         App.setRoot("admin");
+    }
+
+    @FXML
+    FlowPane elementContainer;
+
+    @FXML
+    protected void initialize() {
+        refreshContent();
+    }
+
+    public void refreshContent() {
+        elementContainer.getChildren().clear();
+        for(String f : App.currentDirectoryChildren){   
+            elementContainer.getChildren().add(new Button(f));
+        }
     }
 }
